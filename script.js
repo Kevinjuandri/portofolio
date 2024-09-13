ce47b45f9828ac261f62e2b3d87798bb6f083b92
Kevinjuandri <166259203+Kevinjuandri@users.noreply.github.com>
2024-09-13 19:54:38 +0700

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    if (window.innerWidth < 1024) {
-        alert('Untuk tampilan terbaik, silakan lihat situs ini di mode desktop.');
-    }
-});
