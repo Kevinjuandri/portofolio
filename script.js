426231f979955cbbb2f00c3701c759401263ebd6
Kevinjuandri <166259203+Kevinjuandri@users.noreply.github.com>
2024-09-13 19:54:52 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+
+        document.addEventListener("DOMContentLoaded", function() {
+            const guide = document.getElementById('guide');
+            const closeGuide = document.getElementById('close-guide');
+
+            if (window.innerWidth < 1024) {
+                alert('Untuk tampilan terbaik, silakan lihat situs ini di mode desktop.');
+                guide.style.display = 'block';
+            }
+
+            closeGuide.addEventListener('click', function() {
+                guide.style.display = 'none';
+            });
+
+            window.addEventListener('resize', function() {
+                if (window.innerWidth >= 1024) {
+                    guide.style.display = 'none';
+                }
+            });
+        });
