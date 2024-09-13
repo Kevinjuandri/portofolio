0a36bde488f3ebcb6082e825c85941ba1d599f70
Kevinjuandri <166259203+Kevinjuandri@users.noreply.github.com>
2024-09-13 19:22:01 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    if (window.innerWidth < 1024) {
+        alert('Untuk tampilan terbaik, silakan lihat situs ini di mode desktop.');
+    }
+});
